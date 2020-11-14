f445af742cbcd19961b8782b73b94b0704c23f3b
Sergio Vicente Perales Vidal <sperales@hotmail.com>
2020-11-14 23:47:04 +0100

    redit packageinstllation.sh

--- PackageInstallation.sh
+++ PackageInstallation.sh
@@ -1,5 +1,6 @@
 #Setup
-#   1. 3 Virtual machines Ubuntu for the cluster. 1 master, 2 nodes.
+#   1. 3 Virtual machines Ubuntu for AKS cluster. 1 master, 2 nodes.
 #   2. Static IPs on Azure VMs
 #   3. Swap must be deisable
 
+swappoff -a
